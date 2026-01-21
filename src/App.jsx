943b600326472d259540e54bcdb784b54aa25d75
RohitPatilexe <rohitypatil2004@gmail.com>
2026-01-21 20:30:42 +0530

    few functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,21 @@ import './App.css'
 
 function App() {
   const [text,setText]=useState("");
+  const [toDos,setToDos]=useState(["Wake up","Brush Teeth Nigger"])
+  function addTo(){
+    setToDos([...toDos,text]);
+    setText("");
+  }
   return (
     <div>
       <h1>My To-Do-List</h1>
       <input type="text" value={text} onChange={(e)=>setText(e.target.value)}title='Add a text here'/>
-      <button>Add</button>
-      <p>Echo:{text}</p>
+      <button onClick={addTo}>Add</button>
+      <ul>
+        {toDos.map((todo)=>{
+          return <li>{todo}</li>
+        })}
+      </ul>
     </div>
   )
 }
