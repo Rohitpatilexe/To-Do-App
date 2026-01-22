ee2eaf9b2a4ec27ae7636811868835b0d843cd9a
RohitPatilexe <rohitypatil2004@gmail.com>
2026-01-22 10:49:25 +0530

    Learnt how enter key should be implemented in the input box

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,28 @@ function App() {
     setToDos([...toDos,text]);
     setText("");
   }
+  function toDelete(indexToDelete){
+    const newList=toDos.filter((__,index)=>index!==indexToDelete);
+    setToDos(newList);
+  }
   return (
     <div>
       <h1>My To-Do-List</h1>
-      <input type="text" value={text} onChange={(e)=>setText(e.target.value)}title='Add a text here'/>
+      <input type="text" value={text} onChange={(e)=>setText(e.target.value)} onKeyDown={(e)=>{if(e.key=="Enter")addTo()}} placeholder='Add a text here'/>
       <button onClick={addTo}>Add</button>
       <ul>
-        {toDos.map((todo)=>{
-          return <li>{todo}</li>
-        })}
+        {toDos.map((todo,index)=>{
+          return (
+            
+        <li key={index}>
+          {todo}
+           <button onClick={()=>toDelete(index)} style={{ marginLeft: "10px", color: "red" }}>Delete</button>
+        </li>
+          )
+          })}
       </ul>
     </div>
   )
 }
 
-export default App
+export default App  
